51634980234d16376330306e25adedbeceda13b9
ElderLiniker <eldervhs2013@hotmail.com>
2024-08-06 22:00:08 -0300

    meu linktree

---
+++ script.js
@@ -0,0 +1,55 @@
+const visualizar = document.querySelector(".contagem")
+const visualzarwhta = document.querySelector(".link-whatsaap")
+const facebook = document.querySelector(".link-facebook")
+const linkedin = document.querySelector(".link-linkedin")
+
+
+
+let result = 0
+
+let zerar = 0
+
+let seila = 0
+
+let pois = 0
+
+
+
+
+function thevisualite (){
+
+   
+   
+zerar++
+
+visualizar.innerHTML = zerar
+
+
+
+}
+
+function visualthis (){
+ 
+
+    result++
+    
+    visualzarwhta.innerHTML = result
+
+    }
+
+    function linkedinnn(){
+
+      
+    seila++
+    
+    linkedin.innerHTML = seila
+    }
+
+    function linkfacce (){
+
+      
+    pois++
+    
+    facebook.innerHTML = pois
+    
+    }
